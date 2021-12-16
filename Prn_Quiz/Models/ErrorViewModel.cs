823dae701158f80352bec7153de033649e6275cb
Devlogs <tiendvlp@gmail.com>
2021-12-16 21:28:39 +0700

    init

---
+++ Prn_Quiz/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Prn_Quiz.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
